09b0c8180dc7086d3fcd012175508e9bf01a9bc8
goldenstein64 <goldnstein64@gmail.com>
2023-11-22 20:32:25 -0500

    fix: replaced inspect dep with dkjson

--- lsp-lib/io/debug.lua
+++ lsp-lib/io/debug.lua
@@ -1,4 +1,3 @@
-local inspect = require("inspect")
 local json = require("dkjson")
 
 local logMessage = require("routes.window.logMessage")
@@ -11,7 +10,8 @@ do
 	function debug.read(data)
 		local message
 		if data.id then
-			message = READ_REQUEST_FORMAT:format(data.id, data.method)
+			local displayId = data.id == json.null and "null" or data.id
+			message = READ_REQUEST_FORMAT:format(displayId, data.method)
 		else
 			message = READ_NOTIFICATION_FORMAT:format(data.method)
 		end
@@ -23,13 +23,28 @@ do
 	local WRITE_RESPONSE_RESULT_FORMAT = "<== %s: %s"
 	local WRITE_RESPONSE_ERROR_FORMAT = "<==!!= %s: %s"
 	local WRITE_NOTIFICATION_FORMAT = "<-- %s"
+
+	local displayResultOptions = {
+		indent = true,
+		exception = function(reason, value, _, defaultMessage)
+			if reason == "custom encoder failed" then
+				return tostring(value)
+			elseif reason == "unsupported type" then
+				return tostring(value)
+			else
+				return nil, defaultMessage
+			end
+		end
+	}
+
 	function debug.write(data)
 		local message
 
 		local displayId = data.id == json.null and "null" or data.id
 
 		if data.result then
-			message = WRITE_RESPONSE_RESULT_FORMAT:format(displayId, inspect(data.result))
+			local displayResult = json.encode(data.result, displayResultOptions)
+			message = WRITE_RESPONSE_RESULT_FORMAT:format(displayId, displayResult)
 		elseif data.error then
 			message = WRITE_RESPONSE_ERROR_FORMAT:format(displayId, data.error.message)
 		elseif data.method ~= "window/logMessage" then
