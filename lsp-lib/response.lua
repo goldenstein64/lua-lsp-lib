0bc57f0f1f1a37cb9eb7274684aa1d9cf8dedadf
goldenstein64 <goldnstein64@gmail.com>
2023-11-24 01:58:54 -0500

    style: changed noop callbacks

--- lsp-lib/response.lua
+++ lsp-lib/response.lua
@@ -1,5 +1,7 @@
 local json = require("cjson")
 
+local noop = function() end
+
 ---@type lsp*.Response
 local defaults = {}
 
@@ -7,8 +9,12 @@ defaults["initialize"] = function(params)
 	return { capabilities = {} }
 end
 
+defaults["initialized"] = noop
+
 defaults["shutdown"] = function(params)
 	return json.null
 end
+
+defaults["exit"] = noop
 
 return setmetatable({}, { __index = defaults }) --[[@as lsp*.Response]]
