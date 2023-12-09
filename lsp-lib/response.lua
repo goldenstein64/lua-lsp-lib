bd7300414e43286c41f7f7b98a4588e5a38777c8
goldenstein64 <goldnstein64@gmail.com>
2023-12-09 03:12:20 -0500

    doc: lsp-lib.response

--- lsp-lib/response.lua
+++ lsp-lib/response.lua
@@ -17,4 +17,19 @@ end
 
 defaults["exit"] = noop
 
-return setmetatable({}, { __index = defaults }) --[[@as lsp*.Response]]
+---a key-value mapping of LSP methods to functions that get called when a
+---request or notification with that method is received
+---
+---There are default implementations for the `initialize`, `shutdown` and
+---`exit` methods, but it is recommended to at least implement `initialize` so
+---that the server's capabilities can be modified.
+---
+---All the logic for filtering requests and closing when the `exit`
+---notification is received is already implemented, but implementations for
+---e.g. storing and changing data is left up to the user.
+---@class lsp*.Response
+local response = {}
+
+setmetatable(response, { __index = defaults })
+
+return response
