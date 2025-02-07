7d3b2fa8aef3376871f8a3233135ef692bee42ea
goldenstein64 <goldnstein64@gmail.com>
2025-02-06 19:04:33 -0500

    style: stdio subclasses Provider

--- lsp-lib/io/stdio.lua
+++ lsp-lib/io/stdio.lua
@@ -1,18 +1,21 @@
-local io_std = {
+---represents an I/O provider that reads from `stdin` and writes to `stdout`
+---@class lsp*.io.StdIOProvider : lsp*.io.Provider
+local stdio = {
 	line_ending = "\n",
 }
 
+---reads `bytes` bytes from `stdin`, returning a string
 ---@param bytes integer
 ---@return string
-function io_std:read(bytes)
+function stdio:read(bytes)
 	return io.read(bytes)
 end
 
+---writes `data` to `stdout`
 ---@param data string
-function io_std:write(data)
+function stdio:write(data)
 	io.write(data)
 	io.flush()
 end
 
----@type lsp*.io.Provider
-return io_std
+return stdio
