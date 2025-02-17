bedbb4ef8459404ed6fff4e97bfcddabad096bfc
goldenstein64 <goldnstein64@gmail.com>
2025-02-17 04:46:04 -0500

    doc: generate_base tries looking in PATH for LuaLS

--- scripts/doc/generate_base.lua
+++ scripts/doc/generate_base.lua
@@ -1,12 +1,45 @@
-local language_server_path = ...
-if package.config:sub(1, 1) == "\\" then -- on Windows
-	language_server_path = language_server_path
-		or "%USERPROFILE%\\.vscode\\extensions\\sumneko.lua-3.13.6-win32-x64\\server\\bin\\lua-language-server.exe"
+---@param cmd string
+---@return string
+local function exec(cmd)
+	local prog = assert(io.popen("where lua-language-server.exe"))
+	local output = prog:read("a")
+	prog:close()
+	if output == nil then
+		return ""
+	elseif type(output) ~= "string" then
+		output = tostring(output)
+	end
+	---@cast output string
+	return string.match(output, "^%s*(.-)%s*$")
 end
+
+---@return string?
+local function detect_language_server_path()
+	-- try to detect the language server path
+	if package.config:sub(1, 1) == "\\" then -- on Windows
+		-- just try to find it on the path
+		local path = exec("where lua-language-server"):match("^[^\n]*")
+		if path ~= "" then
+			return path
+		else -- too lazy to figure out what else to do
+			return nil
+		end
+	else -- on Linux-ish
+		-- just try to find it on the path
+		local path = exec("which lua-language-server"):match("^[^\n]*")
+		if path ~= "" then
+			return path
+		else -- too lazy to figure out what else to do
+			return nil
+		end
+	end
+end
+
+local language_server_path = ... or detect_language_server_path()
 
 assert(
 	language_server_path,
-	"pass the language server path as the first argument"
+	"unable to find the language server path; pass it as the first argument"
 )
 
 os.execute(
